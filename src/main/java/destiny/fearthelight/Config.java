248b191e3dadd8fe67a881309b5b580b91cbb198
SGUDestiny <panormov2006@gmail.com>
2026-02-14 16:31:13 +0400

    Proper daybreak length

--- src/main/java/destiny/fearthelight/Config.java
+++ src/main/java/destiny/fearthelight/Config.java
@@ -6,10 +6,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = FearTheLight.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config
-{
-    public enum DaybreakModes
-    {
+public class Config {
+    public enum DaybreakModes {
         CHANCE,
         COUNTDOWN
     }
@@ -17,39 +15,50 @@ public class Config
 
     private static final ForgeConfigSpec.EnumValue<DaybreakModes> DAYBREAK_MODE = BUILDER
             .comment("The mode of Daybreak event")
-            .defineEnum("daybreak_mode", DaybreakModes.COUNTDOWN);
+            .comment("Default: CHANCE")
+            .defineEnum("daybreak_mode", DaybreakModes.CHANCE);
+
+    private static final ForgeConfigSpec.IntValue DAYBREAK_LENGTH_MIN = BUILDER
+            .comment("Number of the least amount of days the Daybreak lasts for. Must not be equal or greater than maximum amount of days")
+            .comment("Default: 10")
+            .defineInRange("daybreak_length_min", 10,1, Integer.MAX_VALUE);
+
+    private static final ForgeConfigSpec.IntValue DAYBREAK_LENGTH_MAX = BUILDER
+            .comment("Number of the maximum amount of days the Daybreak lasts for. Must not be equal or lower than minimum amount of days")
+            .comment("Default: 20")
+            .defineInRange("daybreak_length_max", 20,1, Integer.MAX_VALUE);
 
     private static final ForgeConfigSpec.DoubleValue DAYBREAK_STARTING_CHANCE = BUILDER
             .comment("Starting chance of the Daybreak happening")
+            .comment("Default: 0.1")
             .defineInRange("daybreak_starting_chance", 0.1f,0f, 1f);
 
     private static final ForgeConfigSpec.DoubleValue DAYBREAK_ADDITIVE_CHANCE = BUILDER
             .comment("Number added to chance of Daybreak each passing day")
+            .comment("Default: 0.1")
             .defineInRange("daybreak_additive_chance", 0.1f,0f, 1f);
 
-    private static final ForgeConfigSpec.IntValue DAYBREAK_TIMER = BUILDER
+    private static final ForgeConfigSpec.IntValue DAYBREAK_COUNTDOWN = BUILDER
             .comment("Time in days until Daybreak begins")
-            .defineInRange("daybreak_timer", 1,0, Integer.MAX_VALUE);
-
-    private static final ForgeConfigSpec.IntValue DAYBREAK_LENGTH_MULTIPLIER = BUILDER
-            .comment("Numeric multiplier of how long Daybreak lasts for")
-            .defineInRange("daybreak_length_multiplier", 10,1, Integer.MAX_VALUE);
+            .comment("Default: 1")
+            .defineInRange("daybreak_countdown", 1,0, Integer.MAX_VALUE);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static DaybreakModes daybreakMode;
     public static double daybreakStartingChance;
     public static double daybreakAdditiveChance;
-    public static int daybreakTimer;
-    public static int daybreakLength;
+    public static int daybreakCountdown;
+    public static int daybreakLengthMin;
+    public static int daybreakLengthMax;
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event)
-    {
+    static void onLoad(final ModConfigEvent event) {
         daybreakMode = DAYBREAK_MODE.get();
         daybreakStartingChance = DAYBREAK_STARTING_CHANCE.get();
         daybreakAdditiveChance = DAYBREAK_ADDITIVE_CHANCE.get();
-        daybreakTimer = DAYBREAK_TIMER.get();
-        daybreakLength = DAYBREAK_LENGTH_MULTIPLIER.get();
+        daybreakCountdown = DAYBREAK_COUNTDOWN.get();
+        daybreakLengthMin = DAYBREAK_LENGTH_MIN.get();
+        daybreakLengthMax = DAYBREAK_LENGTH_MAX.get();
     }
 }
--- src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
+++ src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
@@ -7,31 +7,36 @@ import destiny.fearthelight.common.network.packets.DaybreakUpdatePacket;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.util.INBTSerializable;
 
-import java.util.Random;
-
 public class DaybreakCapability implements INBTSerializable<CompoundTag> {
+    public static final String PREVIOUS_DAY = "previousDay";
     public static final String CURRENT_DAY = "currentDay";
     public static final String DAYBREAK_BEGIN_DAY = "daybreakBeginDay";
     public static final String IS_DAY_BROKEN = "isDayBroken";
     public static final String DAYBREAK_CHANCE = "daybreakChance";
+    public static final String DAYBREAK_LENGTH = "daybreakLength";
 
-    public int currentDay = 0;
-    public int daybreakBeginDay = 0;
     public boolean isDayBroken = false;
+    public long previousDay = -1;
+    public int currentDay = 0;
     public float daybreakChance = ((float) Config.daybreakStartingChance);
+    public int daybreakBeginDay = 0;
+    public int daybreakLength = 0;
 
     public void tick(Level level) {
         if (level.isClientSide() || level.getServer() == null) return;
 
         float timeOfDay = level.dimensionType().timeOfDay(level.getDayTime());
+        long calculatedDay = level.getDayTime() / 24000L;
 
-        if (timeOfDay == 0.5) {
+        if (timeOfDay == 0.5 && calculatedDay > previousDay) {
             System.out.println("TIME CORRECT");
 
+            previousDay = calculatedDay;
             currentDay++;
 
             if (!isDayBroken) {
@@ -39,32 +44,36 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
                     RandomSource random = level.getRandom();
 
                     if (random.nextDouble() > 1 - daybreakChance) {
+                        System.out.println("Rolled success");
                         daybreakBegin(level);
                     } else {
+                        System.out.println("Rolled failure");
                         daybreakChance += (float) Config.daybreakAdditiveChance;
                     }
                 }
 
                 if (Config.daybreakMode.equals(Config.DaybreakModes.COUNTDOWN)) {
-                    if (currentDay >= Config.daybreakTimer) {
+                    if (currentDay >= Config.daybreakCountdown) {
                         daybreakBegin(level);
                     }
                 }
             } else {
-                if (currentDay >= daybreakBeginDay + Config.daybreakLength) {
+                if (currentDay >= daybreakBeginDay + daybreakLength) {
                     daybreakEnd(level);
                 }
             }
         }
 
-        System.out.println("Time of Day: " + timeOfDay);
-        System.out.println("Current Day: " + currentDay);
-        System.out.println("Chance: " + daybreakChance);
+        //System.out.println("Time of Day: " + timeOfDay);
+        //System.out.println("Current Day: " + currentDay);
+        //System.out.println("Chance: " + daybreakChance);
     }
 
     public void daybreakEnd(Level level) {
         isDayBroken = false;
         daybreakBeginDay = 0;
+        daybreakLength = 0;
+        daybreakChance = ((float) Config.daybreakStartingChance);
         ModNetwork.sendPacketToDimension(level.dimension(), new DaybreakUpdatePacket(isDayBroken));
         for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true)) {
             DaybreakStartCriterion.DAYBREAK_FINISH.trigger(player);
@@ -75,6 +84,7 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
         isDayBroken = true;
         daybreakBeginDay = currentDay;
         daybreakChance = ((float) Config.daybreakStartingChance);
+        daybreakLength = Mth.nextInt(level.getRandom(), Config.daybreakLengthMin, Config.daybreakLengthMax);
         ModNetwork.sendPacketToDimension(level.dimension(), new DaybreakUpdatePacket(isDayBroken));
         for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true)) {
             DaybreakStartCriterion.DAYBREAK_START.trigger(player);
@@ -84,18 +94,22 @@ public class DaybreakCapability implements INBTSerializable<CompoundTag> {
     @Override
     public CompoundTag serializeNBT() {
         CompoundTag tag = new CompoundTag();
-        tag.putInt(CURRENT_DAY, currentDay);
-        tag.putInt(DAYBREAK_BEGIN_DAY, daybreakBeginDay);
         tag.putBoolean(IS_DAY_BROKEN, isDayBroken);
+        tag.putLong(PREVIOUS_DAY, previousDay);
+        tag.putInt(CURRENT_DAY, currentDay);
         tag.putFloat(DAYBREAK_CHANCE, daybreakChance);
+        tag.putInt(DAYBREAK_BEGIN_DAY, daybreakBeginDay);
+        tag.putInt(DAYBREAK_LENGTH, daybreakLength);
         return tag;
     }
 
     @Override
     public void deserializeNBT(CompoundTag tag) {
-        this.currentDay = tag.getInt(CURRENT_DAY);
-        this.daybreakBeginDay = tag.getInt(DAYBREAK_BEGIN_DAY);
         this.isDayBroken = tag.getBoolean(IS_DAY_BROKEN);
+        this.previousDay = tag.getLong(PREVIOUS_DAY);
+        this.currentDay = tag.getInt(CURRENT_DAY);
         this.daybreakChance = tag.getFloat(DAYBREAK_CHANCE);
+        this.daybreakBeginDay = tag.getInt(DAYBREAK_BEGIN_DAY);
+        this.daybreakLength = tag.getInt(DAYBREAK_BEGIN_DAY);
     }
 }

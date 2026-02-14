6b4a1e4af3eabaf01f551d3c5c0c47238bf1840f
SGUDestiny <panormov2006@gmail.com>
2026-02-14 15:22:17 +0400

    Cleaning up code

--- src/main/java/destiny/fearthelight/FearTheLight.java
+++ src/main/java/destiny/fearthelight/FearTheLight.java
@@ -17,53 +17,35 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(FearTheLight.MODID)
-public class FearTheLight
-{
-    // Define mod id in a common place for everything to reference
+public class FearTheLight {
     public static final String MODID = "fearthelight";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
 
-    public FearTheLight()
-    {
+    public FearTheLight() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModAdvancements.register();
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
+    private void commonSetup(final FMLCommonSetupEvent event) {
         ModNetwork.registerPackets();
-        LOGGER.info("HELLO FROM COMMON SETUP");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void registerDimensionEffects(RegisterDimensionSpecialEffectsEvent event)
-        {
+        public static void registerDimensionEffects(RegisterDimensionSpecialEffectsEvent event) {
             //event.register(DaybreakOverworldEffects.OVERWORLD_EFFECTS, new DaybreakOverworldEffects());
         }
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
         }
     }
 }
--- src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
+++ src/main/java/destiny/fearthelight/common/daybreak/DaybreakCapability.java
@@ -7,103 +7,95 @@ import destiny.fearthelight.common.network.packets.DaybreakUpdatePacket;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.util.INBTSerializable;
 
 import java.util.Random;
 
 public class DaybreakCapability implements INBTSerializable<CompoundTag> {
-    public static final String DAYBREAK_CURRENT_DAY = "daybreakCurrentDay";
-    public static final String DAYBREAK_STARTED_AT = "daybreakStartedAt";
+    public static final String CURRENT_DAY = "currentDay";
+    public static final String DAYBREAK_BEGIN_DAY = "daybreakBeginDay";
     public static final String IS_DAY_BROKEN = "isDayBroken";
-    public static final String CHANCE = "daybreakChance";
+    public static final String DAYBREAK_CHANCE = "daybreakChance";
 
     public int currentDay = 0;
-    public int dayStartedAt = 0;
+    public int daybreakBeginDay = 0;
     public boolean isDayBroken = false;
-    public float chance = ((float) Config.daybreakStartingChance);
+    public float daybreakChance = ((float) Config.daybreakStartingChance);
 
     public void tick(Level level) {
-        if(level.isClientSide() || level.getServer() == null) {
-            return;
-        }
+        if (level.isClientSide() || level.getServer() == null) return;
+
         float timeOfDay = level.dimensionType().timeOfDay(level.getDayTime());
 
-        System.out.println("Time of Day: " + timeOfDay);
-        System.out.println("Current Day: " + currentDay);
-        System.out.println("Chance: " + (chance));
-
-        if(timeOfDay == 0.5)
-        {
+        if (timeOfDay == 0.5) {
             System.out.println("TIME CORRECT");
 
             currentDay++;
-            if(Config.daybreakMode.equals(Config.DaybreakModes.CHANCE) && calculateChance())
-            {
-                daybreakTrigger(level);
-            }
-            else if(Config.daybreakMode.equals(Config.DaybreakModes.COUNTDOWN) && currentDay >= Config.daybreakTimer && !isDayBroken)
-            {
-                daybreakTrigger(level);
-            }
-            if(currentDay >= dayStartedAt+Config.daybreakLength && isDayBroken)
-            {
-                daybreakUntrigger(level);
+
+            if (!isDayBroken) {
+                if (Config.daybreakMode.equals(Config.DaybreakModes.CHANCE)) {
+                    RandomSource random = level.getRandom();
+
+                    if (random.nextDouble() > 1 - daybreakChance) {
+                        daybreakBegin(level);
+                    } else {
+                        daybreakChance += (float) Config.daybreakAdditiveChance;
+                    }
+                }
+
+                if (Config.daybreakMode.equals(Config.DaybreakModes.COUNTDOWN)) {
+                    if (currentDay >= Config.daybreakTimer) {
+                        daybreakBegin(level);
+                    }
+                }
+            } else {
+                if (currentDay >= daybreakBeginDay + Config.daybreakLength) {
+                    daybreakEnd(level);
+                }
             }
         }
+
+        System.out.println("Time of Day: " + timeOfDay);
+        System.out.println("Current Day: " + currentDay);
+        System.out.println("Chance: " + daybreakChance);
     }
 
-    public boolean calculateChance()
-    {
-        if(isDayBroken)
-            return false;
-
-        Random random = new Random();
-        if(random.nextDouble() > 1-chance)
-            return true;
-        else
-            chance += ((float) Config.daybreakAdditiveChance);
-        return false;
-    }
-
-    public void daybreakUntrigger(Level level)
-    {
+    public void daybreakEnd(Level level) {
         isDayBroken = false;
-        dayStartedAt = 0;
+        daybreakBeginDay = 0;
         ModNetwork.sendPacketToDimension(level.dimension(), new DaybreakUpdatePacket(isDayBroken));
-        for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true))
-        {
+        for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true)) {
             DaybreakStartCriterion.DAYBREAK_FINISH.trigger(player);
         }
     }
 
-    public void daybreakTrigger(Level level)
-    {
+    public void daybreakBegin(Level level) {
         isDayBroken = true;
-        dayStartedAt = currentDay;
-        chance = ((float) Config.daybreakStartingChance);
+        daybreakBeginDay = currentDay;
+        daybreakChance = ((float) Config.daybreakStartingChance);
         ModNetwork.sendPacketToDimension(level.dimension(), new DaybreakUpdatePacket(isDayBroken));
-        for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true))
-        {
+        for(ServerPlayer player : ((ServerLevel) level).getPlayers(serverPlayer -> true)) {
             DaybreakStartCriterion.DAYBREAK_START.trigger(player);
         }
     }
 
     @Override
-    public CompoundTag serializeNBT()
-    {
+    public CompoundTag serializeNBT() {
         CompoundTag tag = new CompoundTag();
-        tag.putInt(DAYBREAK_CURRENT_DAY, currentDay);
-        tag.putInt(DAYBREAK_STARTED_AT, dayStartedAt);
+        tag.putInt(CURRENT_DAY, currentDay);
+        tag.putInt(DAYBREAK_BEGIN_DAY, daybreakBeginDay);
         tag.putBoolean(IS_DAY_BROKEN, isDayBroken);
+        tag.putFloat(DAYBREAK_CHANCE, daybreakChance);
         return tag;
     }
 
     @Override
-    public void deserializeNBT(CompoundTag tag)
-    {
-        this.currentDay = tag.getInt(DAYBREAK_CURRENT_DAY);
-        this.dayStartedAt = tag.getInt(DAYBREAK_STARTED_AT);
+    public void deserializeNBT(CompoundTag tag) {
+        this.currentDay = tag.getInt(CURRENT_DAY);
+        this.daybreakBeginDay = tag.getInt(DAYBREAK_BEGIN_DAY);
         this.isDayBroken = tag.getBoolean(IS_DAY_BROKEN);
+        this.daybreakChance = tag.getFloat(DAYBREAK_CHANCE);
     }
 }
